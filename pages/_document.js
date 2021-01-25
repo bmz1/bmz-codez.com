73413672090ed559dda98e11b088704433578d4b
Mate Boer <mate.boer@logmein.com>
2021-01-25 21:50:39 +0100

    fix google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,17 @@ export default class MyDocument extends Document {
       <Html lang={siteMetadata.language}>
         <Head>
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-155334110-1"
+          ></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155334110-1"></script>
-              <script>
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'UA-155334110-1');
-              </script>
         `,
             }}
           />
